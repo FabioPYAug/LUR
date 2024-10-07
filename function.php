0381d78fb0c17391d7aa3c5a5627e0012ffa2b0a
FabioPYAug <fabioacv2403@gmail.com>
2024-10-07 17:15:23 -0300

    AGORA COM FUNCIONANDO A PESQUISA AUTOMÁTICA!

--- function.php
+++ function.php
@@ -1,5 +1,46 @@
 <?php 
-
 include("conexao.php");
+if(isset($_POST["input"])){
 
+    $input = trim($_POST["input"]); // Remove espaços em branco
+
+    if(empty($input)) {
+        echo "<h6 class='text-danger text-center'>Por favor, digite algo para pesquisar.</h6>";
+        exit;
+    }
+
+    $query = "SELECT * FROM ne_dados WHERE nome LIKE '{$input}%' OR id LIKE '{$input}%' OR tipo LIKE '{$input}%'";
+    $result = mysqli_query($conexao, $query);
+
+    if(mysqli_num_rows($result) > 0){?>
+        <table class="table table-borderline table-striped mt-4">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NOME</th>
+                    <th>TIPO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                while($row = mysqli_fetch_assoc($result)){
+                    $id = $row['ID'];
+                    $nome = $row['nome'];
+                    $tipo = $row['tipo'];
+                ?>
+                <tr>
+                    <td><?php echo $id;?></td>
+                    <td><?php echo $nome;?></td>
+                    <td><?php echo $tipo;?></td>
+                </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    <?php
+    } else {
+        echo "<h6 class='text-danger text-center'>Zero Dados Encontrados</h6>";
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-
-<title>Noite Escura</title>
-<?php include "./style.php"; ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
+    <title>Noite Escura</title>
+    <?php include "./style.php"; ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
-
 <body>
     <div class="search">
         <label for="searchInput">
@@ -15,34 +12,30 @@
         </label>
         <input type="text" id="searchInput" placeholder="Pesquisar">
     </div>
-    <div id="resultado">
-
-    </div>
-    <script type ="text/javascript">
+    <div id="resultado"></div>
+    <script type="text/javascript">
         $(document).ready(function(){
-
             $("#searchInput").keyup(function(){
-                var input = $(this).val();
+                var input = $(this).val().trim(); // Remove espaços em branco
                 if(input != ""){
                     $.ajax({
                         url: "function.php",
                         method: "POST",
-                        data: {input:input},
-
-                        success:function(data){
-                            $("#resultado").html(data);
+                        data: {input: input},
+                        success: function(data) {
+                            console.log(data); // Para depuração
+                            if(data.trim() != "") {
+                                $("#resultado").html(data).css("display", "block"); // Mostra o resultado
+                            } else {
+                                $("#resultado").css("display", "none"); // Oculta se não houver resultados
+                            }
                         }
-
-                    })
-                }else{
-                    $("#resultado").css("display", "none");
+                    });
+                } else {
+                    $("#resultado").css("display", "none"); // Oculta se a entrada estiver vazia
                 }
-            })
+            });
         });
     </script>
 </body>
-
 </html>
-<?php
-
-?>
