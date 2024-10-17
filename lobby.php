53978db8db84c3d45216fb933543c56e64fd3c84
FabioPYAug <fabioacv2403@gmail.com>
2024-10-17 17:19:30 -0300

    COMEÇO DA HOME DE NOITE ESCURA

--- lobby.php
+++ lobby.php
@@ -3,19 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parallax com Sol e Árvores</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="lobby.css">
 </head>
 <body>
-    <div class="parallax">
-        <!-- Camada das árvores -->
-        <div class="trees"></div>
-        <!-- Camada do sol -->
-        <div class="sun"></div>
-    </div>
-    <div class="content">
-        <h1>Bem-vindo ao Efeito Parallax</h1>
-        <p>Role para ver o efeito!</p>
-    </div>
+
+    <header>
+        <h1>Logo</h1>
+        <nav>
+            <a href="#">Retorno</a>
+            <a href="#">Pesquisa</a>
+            <a href="#">Mapa</a>
+            <a href="#">Perfil</a>
+        </nav>
+    </header>
+    
+    <section class="NE">
+        
+    </section>
+
 </body>
 </html>
