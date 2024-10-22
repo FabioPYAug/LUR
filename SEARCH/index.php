aa16c9dacf6d18494b08384822deb22f9f2a4ad6
FabioPYAug <fabioacv2403@gmail.com>
2024-10-22 16:51:28 -0300

    ATUALIZAÇÃO FINAL DE HOJE

--- SEARCH/index.php
+++ SEARCH/index.php
@@ -46,27 +46,67 @@ include 'style.php';
     </section>
 
     <script type="text/javascript">
-        $(document).ready(function () {
-            $("#searchInput").keyup(function () {
-                var input = $(this).val().trim();
-                if (input != "") {
+        $(document).ready(function(){
+            $("#searchInput").keyup(function(){
+                var input = $(this).val().trim(); 
+                if(input != ""){
                     $.ajax({
                         url: "function.php",
                         method: "POST",
-                        data: { input: input },
-                        success: function (data) {
-                            if (data.trim() != "") {
-                                $("#resultado").html(data).css("display", "block");
+                        data: {input: input},
+                        success: function(data) {
+                            if(data.trim() != "") {
+                                $("#resultado").html(data).css("display", "block"); 
                             } else {
-                                $("#resultado").css("display", "none");
+                                $("#resultado").css("display", "none"); 
                             }
                         }
                     });
                 } else {
-                    $("#resultado").css("display", "none");
+                    $("#resultado").css("display", "none"); 
                 }
             });
         });
+
+        function valores(nome, tipo, alcance, descricao, historia, entidade, teste, dano, critico, peso, tipoDano, venda, defesa, penalidade, acao, efeito){
+            let LocalImagem = `../Imagens/PlaceHolder.png`;
+
+            sessionStorage.setItem('nome', nome);
+            sessionStorage.setItem('tipo', tipo);
+            sessionStorage.setItem('alcance', alcance);
+            sessionStorage.setItem('descricao', descricao);
+            sessionStorage.setItem('historia', historia);
+            sessionStorage.setItem('entidade', entidade);
+            sessionStorage.setItem('teste', teste);
+            sessionStorage.setItem('dano', dano);
+            sessionStorage.setItem('critico', critico);
+            sessionStorage.setItem('peso', peso);
+            sessionStorage.setItem('tipoDano', tipoDano);
+            sessionStorage.setItem('venda', venda);
+            sessionStorage.setItem('defesa', defesa);
+            sessionStorage.setItem('penalidade', penalidade);
+            sessionStorage.setItem('LocalImagem', LocalImagem);
+            sessionStorage.setItem('acao', acao);
+            sessionStorage.setItem('efeito', efeito);
+            switch (tipo){
+                case "Arma":
+                    window.location.href = 'Valores/ArmaPadrão.html'
+                    break;
+                case "Material":
+                    window.location.href = 'Valores/MaterialPadrão.html'
+                    break;
+                case "Poção":
+                    window.location.href = 'Valores/PoçãoPadrão.html'
+                    break;
+                case "Moeda":
+                    window.location.href = 'Valores/MoedaPadrão.html'
+                    break;
+                case "Itens":
+                    window.location.href = 'Valores/ItensPadrão.html'
+                    break;
+            }
+            
+        }
     </script>
 </body>
 </html>
