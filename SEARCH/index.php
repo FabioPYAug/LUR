fdbd4f3972e6ca9405e72bb2e32d9c447be93748
FabioPYAug <fabioacv2403@gmail.com>
2024-10-22 14:22:46 -0300

    MODIFICAÇÃO OD SEARCH

--- SEARCH/index.php
+++ SEARCH/index.php
@@ -1,166 +1,78 @@
+<?php 
+include 'style.php';
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
+
 <head>
-    <title>Noite Escura</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <title>Noite Escura - Menu de Busca</title>
+    <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
+
 <body>
+    <header id="header">
+        <div class="container">
+            <div class="flex">
+                <a href="#"><i class="bi bi-globe-americas"></i></a>
+                <div class="btn-contato">
+                    <a onclick="window.location.href='../lobby.php'"><button>Retornar</button></a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="banner banner-1">
+        <div class="centered-text">
+            <h1>PESQUISA EM <span>NOITE ESCURA</span></h1>
+        </div>
+    </section>
+
+    <section class="banner banner-2">
     <div class="search">
         <label for="searchInput">
             <span class="material-symbols-outlined"></span>
         </label>
         <input type="text" id="searchInput" placeholder="Pesquisar">
     </div>
+    </section>
     
-    <div id="resultado"></div> 
+
+    <div id="resultado"></div>
 
     <script type="text/javascript">
-        $(document).ready(function(){
-            $("#searchInput").keyup(function(){
-                var input = $(this).val().trim(); 
-                if(input != ""){
+        $(document).ready(function () {
+            $("#searchInput").keyup(function () {
+                var input = $(this).val().trim();
+                if (input != "") {
                     $.ajax({
                         url: "function.php",
                         method: "POST",
-                        data: {input: input},
-                        success: function(data) {
-                            if(data.trim() != "") {
-                                $("#resultado").html(data).css("display", "block"); 
+                        data: { input: input },
+                        success: function (data) {
+                            if (data.trim() != "") {
+                                $("#resultado").html(data).css("display", "block");
                             } else {
-                                $("#resultado").css("display", "none"); 
+                                $("#resultado").css("display", "none");
                             }
                         }
                     });
                 } else {
-                    $("#resultado").css("display", "none"); 
+                    $("#resultado").css("display", "none");
                 }
             });
         });
-
-        function valores(nome, tipo, alcance, descricao, historia, entidade, teste, dano, critico, peso, tipoDano, venda, defesa, penalidade, acao, efeito){
-            let LocalImagem = `../Imagens/PlaceHolder.png`;
-
-            sessionStorage.setItem('nome', nome);
-            sessionStorage.setItem('tipo', tipo);
-            sessionStorage.setItem('alcance', alcance);
-            sessionStorage.setItem('descricao', descricao);
-            sessionStorage.setItem('historia', historia);
-            sessionStorage.setItem('entidade', entidade);
-            sessionStorage.setItem('teste', teste);
-            sessionStorage.setItem('dano', dano);
-            sessionStorage.setItem('critico', critico);
-            sessionStorage.setItem('peso', peso);
-            sessionStorage.setItem('tipoDano', tipoDano);
-            sessionStorage.setItem('venda', venda);
-            sessionStorage.setItem('defesa', defesa);
-            sessionStorage.setItem('penalidade', penalidade);
-            sessionStorage.setItem('LocalImagem', LocalImagem);
-            sessionStorage.setItem('acao', acao);
-            sessionStorage.setItem('efeito', efeito);
-            switch (tipo){
-                case "Arma":
-                    window.location.href = 'Valores/ArmaPadrão.html';
-                    break;
-                case "Material":
-                    window.location.href = 'Valores/MaterialPadrão.html';
-            }
-            
-        }
     </script>
 </body>
 </html>
-<style>
-@import url("https://fonts.googleapis.com/css?family=Inter:400|Jura:400");
-
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Jura', sans-serif;
-    background-color: #f2f2ed;
-}
-
-.search {
-    display: flex;
-    align-items: center;
-    border: 1px solid #ccc;
-    background-color: #00000006;
-    width: 750px;
-    border-radius: 20px;
-    padding: 5px 10px;
-    color: #4b4b4b;
-    margin: 20px auto;
-}
-
-#searchInput {
-    padding: 8px;
-    border: none;
-    background-color: transparent;
-    width: 100%;
-    outline: none;
-    font-size: 20px;
-}
-
-#resultado {
-    max-width: 750px;
-    margin: 20px auto;
-    display: none;
-}
-
-h6 {
-    margin: 20px 0 0 0;
-    padding: 0;
-    box-sizing: border-box;
-    color: black;
-    font-size: 25px;
-    text-align: center; 
-}
-
-.custom-btn {
-  width: 750px;
-  height: 40px;
-  color: black;
-  border-radius: 5px;
-  padding: 10px 25px;
-  font-family: 'Lato', sans-serif;
-  font-weight: 500;
-  background: transparent;
-  cursor: pointer;
-  transition: all 0.3s ease;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  outline: none;
-}
-
-.btn-1 {
-  background: #00000006;
-  border: none;
-}
-
-.btn-1:hover {
-  background: rgb(255, 255, 255);
-  border-color: yellow;
-}
-
-
-.item-left {
-  flex: 1; 
-  text-align: left;
-  background: transparent;
-}
-
-
-.item-center {
-  flex: 20;
-  text-align: left;
-  background: transparent;
-}
-
-.item-right {
-  flex: 1;
-  text-align: right;
-  background: transparent;
-}
-</style>
-
+<script>
+    window.addEventListener("scroll", function () {
+        let header = document.querySelector('#header')
+        header.classList.toggle('rolagem', window.scrollY > 500)
+    })
+</script>
---
+++ SEARCH/style.php
@@ -0,0 +1,188 @@
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+
+html {
+    height: 100%;
+    background-color: #fff; /* Background set to white */
+}
+
+.container {
+    max-width: 1280px;
+    margin: auto;
+}
+
+.flex {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+header {
+    width: 100%;
+    padding: 60px 4%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    transition: .5s;
+    z-index: 1000;
+}
+
+header.rolagem {
+    background-color: #fff;
+    padding: 20px 4%;
+}
+
+header.rolagem a, header.rolagem i {
+    color: #FE775A;
+}
+
+header i {
+    font-size: 30px;
+    color: #fff;
+}
+
+header ul {
+    list-style-type: none;
+}
+
+header ul li {
+    display: inline-block;
+    margin: 0 40px;
+}
+
+header ul li a {
+    color: #000; 
+    text-decoration: none;
+}
+
+.btn-contato button {
+    border-radius: 99px;
+    gap: 30px;
+    font-size: 20px;
+    width: 120px;
+    height: 40px;
+    border: 0;
+    background-color: #FE775A;
+    color: #fff;
+    cursor: pointer;
+    transition: .2s;
+}
+
+.banner {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    position: relative;
+    background-attachment: fixed;
+}
+
+.banner-1 {
+    background-color: #1a1a1a; 
+    color: #fff;
+}
+
+.banner-2 {
+    background-color: #; 
+}
+
+.banner-3 {
+    background-color: #e6b800; 
+}
+
+.banner h1 {
+    font-size: 4em;
+    color: #fff;
+}
+
+.banner h1 span {
+    color: #FFAE81;
+}
+
+.search {
+    display: flex;
+    align-items: center;
+    border: 1px solid #ccc;
+    background-color: #00000006;
+    width: 750px;
+    border-radius: 20px;
+    padding: 5px 10px;
+    color: #4b4b4b;
+    margin: 20px auto;
+}
+
+#searchInput {
+    padding: 8px;
+    border: none;
+    background-color: transparent;
+    width: 100%;
+    outline: none;
+    font-size: 20px;
+}
+
+#resultado {
+    max-width: 750px;
+    margin: 20px auto;
+    display: none;
+}
+
+h6 {
+    margin: 20px 0 0 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: black;
+    font-size: 25px;
+    text-align: center;
+}
+
+.custom-btn {
+    width: 750px;
+    height: 40px;
+    color: black;
+    border-radius: 5px;
+    padding: 10px 25px;
+    font-family: 'Lato', sans-serif;
+    font-weight: 500;
+    background: transparent;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    outline: none;
+}
+
+.btn-1 {
+    background: #00000006;
+    border: none;
+}
+
+.btn-1:hover {
+    background: rgb(255, 255, 255);
+    border-color: yellow;
+}
+
+.item-left {
+    flex: 1;
+    text-align: left;
+    background: transparent;
+}
+
+.item-center {
+    flex: 20;
+    text-align: left;
+    background: transparent;
+}
+
+.item-right {
+    flex: 1;
+    text-align: right;
+    background: transparent;
+}
+</style>
