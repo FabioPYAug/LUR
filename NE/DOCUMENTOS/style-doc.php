eb067abee9bd0b0fd5c17aa4ed672fee777c7da0
FabioPYAug <fabioacv2403@gmail.com>
2024-12-26 17:13:28 -0300

    atualização geral docs

--- NE/DOCUMENTOS/style-doc.php
+++ NE/DOCUMENTOS/style-doc.php
@@ -12,6 +12,7 @@
 html, body {
     height: 100%;
     background-color: #FFF8D6;
+    overflow: hidden;
 }
 
 .container {
@@ -23,67 +24,6 @@ html, body {
     display: flex;
     align-items: center;
     justify-content: space-between;
-}
-
-header {
-    width: 100%;
-    padding: 60px 4%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    transition: .5s;
-    z-index: 1000;
-    background: #F0D8A8;
-}
-
-header.rolagem {
-    background-color: #D5A982;
-    padding: 20px 4%;
-}
-
-header i {
-    font-size: 30px;
-    color: #B07E63;
-}
-
-header.rolagem .btn-contato button {
-    background-color: #CD8861;
-    color: #FFF8D6;
-}
-
-header .btn-contato button {
-    border-radius: 30px;
-    font-size: 20px;
-    width: 120px;
-    height: 40px;
-    border: 0;
-    background-color: #D5A982;
-    color: #4E3E30;
-    cursor: pointer;
-    transition: transform 0.3s ease, box-shadow 0.3s ease, background-color 0.3s ease;
-}
-
-header .btn-contato button:hover {
-    transform: scale(1.1);
-    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.15);
-    background-color: #B07E63;
-}
-
-.banner-1 {
-    background: linear-gradient(160deg, #F0D8A8, #FFF8D6);
-    color: #4E3E30;
-    padding: 100px 0;
-    text-align: center;
-}
-
-.banner-1 h1 {
-    font-size: 4em;
-    font-weight: bold;
-    color: #4E3E30;
-}
-
-.banner-1 h1 span {
-    color: #BA783E;
 }
 
 .banner-2 {
