748710cd1f4490a2e9d62303f01dc39808574d60
FabioPYAug <fabioacv2403@gmail.com>
2025-01-27 17:34:40 -0300

    Começo do perfil

--- PERFIL/pfl-basico.php
+++ PERFIL/pfl-basico.php
@@ -1,29 +1,86 @@
 <?php
-session_start();
-include 'db_connection.php';
-
-$user_id = $_SESSION['user_id'];
-$sql = "SELECT * FROM users WHERE id = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $user_id);
-$stmt->execute();
-$user = $stmt->get_result()->fetch_assoc();
-
-$sql_images = "SELECT * FROM user_images WHERE user_id = ?";
-$stmt_images = $conn->prepare($sql_images);
-$stmt_images->bind_param("i", $user_id);
-$stmt_images->execute();
-$images = $stmt_images->get_result();
+include 'pfl-basico-stl.php';
+include 'pfl-basico-js.php';
 ?>
 
-<h1>Perfil de <?php echo htmlspecialchars($user['username']); ?></h1>
-<img src="<?php echo $user['profile_image'] ?: 'default.jpg'; ?>" alt="Foto de Perfil" width="150">
-<p><?php echo htmlspecialchars($user['description']); ?></p>
+<body>
+    <div class="main-container">
+        <!-- HEADER -->
+        <header class="block">
+            <ul class="header-menu horizontal-list">
+                <li>
+                    <a class="header-menu-tab" href="#settings">
+                        <span class="icon entypo-cog scnd-font-color"></span>Settings
+                    </a>
+                </li>
+                <li>
+                    <a class="header-menu-tab" href="#messages">
+                        <span class="icon fontawesome-envelope scnd-font-color"></span>Messages
+                    </a>
+                </li>
+            </ul>
+        </header>
 
-<h2>Galeria</h2>
-<?php while ($image = $images->fetch_assoc()): ?>
-    <img src="<?php echo $image['image_path']; ?>" alt="Imagem do usuário" width="100">
-<?php endwhile; ?>
+        <!-- CONTAINER DA ESQUERDA -->
+        <div class="left-container container">
+            <div class="menu-box block">
+                <h2 class="titular">Menu</h2>
+                <ul class="menu-box-menu">
+                    <li>
+                        <a class="menu-box-tab" href="#sessoes">
+                            <span class="icon fontawesome-envelope scnd-font-color"></span>Sessões Jogadas
+                            <div class="menu-box-number">24</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="menu-box-tab" href="#criticos">
+                            <span class="icon entypo-paper-plane scnd-font-color"></span>Críticos
+                            <div class="menu-box-number">3</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="menu-box-tab" href="#falhas">
+                            <span class="icon entypo-calendar scnd-font-color"></span>Falhas Críticas
+                            <div class="menu-box-number">5</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="menu-box-tab" href="#personagens">
+                            <span class="icon entypo-cog scnd-font-color"></span>Personagens
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
-<a href="upload.php">Adicionar Imagem</a>
-<a href="logout.php">Logout</a>
+        <!-- CONTAINER DA DIREITA -->
+        <div class="middle-container container">
+            <div class="profile block">
+                <div class="profile-picture big-profile-picture clear">
+                    <img src="https://via.placeholder.com/150" alt="Profile Picture">
+                </div>
+                <h1 class="user-name">Nickname</h1>
+                <div class="profile-description">
+                    <p class="scnd-font-color">Descrição breve do usuário.</p>
+                </div>
+                <ul class="profile-options horizontal-list">
+                    <li>
+                        <a class="comments" href="#comments">
+                            <p><span class="icon fontawesome-comment-alt scnd-font-color"></span>23</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="views" href="#views">
+                            <p><span class="icon fontawesome-eye-open scnd-font-color"></span>841</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="likes" href="#likes">
+                            <p><span class="icon fontawesome-heart-empty scnd-font-color"></span>49</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
