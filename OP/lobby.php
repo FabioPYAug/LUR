ef8d56ee3c4441d8d71cf5d963162fa8b1ab67f0
FabioPYAug <fabioacv2403@gmail.com>
2025-03-10 16:47:59 -0300

    Começo do Menu Paranormal

---
+++ OP/lobby.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="lobby.css">
+</head>
+<body>
+
+    <header>
+        <h1>Logo</h1>
+        <nav>
+            <a onclick="window.location.href='DOCUMENTOS/index-doc.php'">Documentos</a>
+            <a onclick="window.location.href='INVENTARIO/index-inv.php'">Inventário</a>
+            <a >Mapa</a>
+            <a onclick="window.location.href='GALERIA/index-gal.php'">Galeria</a>
+            <a onclick="window.location.href='../PERFIL/pfl-basico.php'">Perfil</a>
+        </nav>
+    </header>
+    
+    <section class="OP">
+        
+    </section>
+
+</body>
+</html>
