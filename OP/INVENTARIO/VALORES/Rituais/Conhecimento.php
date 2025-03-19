5c0ecb25ea48a33a0d7d7858739f8c8d7aac97b1
FabioPYAug <fabioacv2403@gmail.com>
2025-03-19 15:36:08 -0300

    Finalização da aba rituais primaria

---
+++ OP/INVENTARIO/VALORES/Rituais/Conhecimento.php
@@ -0,0 +1,557 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title id="tituloPagina">Mistérios e Rituais</title>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+  <link rel="stylesheet" href="styles.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Cinzel:wght@400;600&display=swap"
+    rel="stylesheet" />
+</head>
+
+<body>
+  <canvas id="space"></canvas>
+  <header class="header">
+    <div class="header-container">
+      <h1 class="site-title" id="RitualNome">[Nome do Ritual]</h1>
+      <button class="back-button" onclick="window.location.href='../index-inv.php'">
+        &larr; Voltar
+      </button>
+    </div>
+  </header>
+
+  <div class="subheader">
+    <nav>
+      <ul>
+        <li><a href="#efeito">Efeito</a></li>
+      </ul>
+    </nav>
+  </div>
+
+  <main class="main-content">
+    <aside class="sidebar"></aside>
+
+    <section class="content">
+      <article>
+      <div class="details-image">
+            <img
+              id="ritualImagem"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3r_sM7QQ45aSu9y8Bug6rJtMB--YWkyAiA&s"
+              alt="Imagem do Ritual"
+            />
+          </div>
+        <section id="efeito">
+          <h2 data-text="Efeito">Efeito</h2>
+          <p id="op_padrao" data-text="">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p id="op_discente" data-text=""><strong>Discente: </strong>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p id="op_verdadeiro" data-text=""><strong>Verdadeiro: </strong>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </section>
+        <br>
+
+        <section id="detalhes">
+          <table class="details-table">
+            <tr>
+              <th>Círculo</th>
+              <td id="RitualCirculo">[Círculo]</td>
+            </tr>
+            <tr>
+              <th>Custo</th>
+              <td id="RitualCusto">[Custo]</td>
+            </tr>
+            <tr>
+              <th>Alvo</th>
+              <td id="RitualAlvo">[Alvo]</td>
+            </tr>
+            <tr>
+              <th>Resistência</th>
+              <td id="RitualResistencia">[Resistência]</td>
+            </tr>
+            <tr>
+              <th>Duração</th>
+              <td id="RitualDuracao">[Duração]</td>
+            </tr>
+            <tr>
+              <th>Alcance</th>
+              <td id="RitualAlcance">[Alcance]</td>
+            </tr>
+          </table>
+          <br />
+          <div class="details-container">
+            <div class="details-text">
+              <p>
+                <strong>Tipo</strong>
+              </p>
+              <p>
+                <strong>Alcance</strong>
+              </p>
+              <p>
+                <strong>Efeito</strong>
+              </p>
+            </div>
+          </div>
+        </section>
+      </article>
+    </section>
+  </main>
+  <script>
+    var canvas;
+    var context;
+    var screenH;
+    var screenW;
+    var stars = [];
+    var fps = 50;
+    var numStars = 200;
+
+    $('document').ready(function () {
+
+      screenH = $(window).height();
+      screenW = $(window).width();
+
+      canvas = $('#space');
+
+      canvas.attr('height', screenH);
+      canvas.attr('width', screenW);
+      context = canvas[0].getContext('2d');
+
+      for (var i = 0; i < numStars; i++) {
+        var x = Math.round(Math.random() * screenW);
+        var y = Math.round(Math.random() * screenH);
+        var length = 1 + Math.random() * 3;
+        var opacity = Math.random();
+
+        var star = new Star(x, y, length, opacity);
+
+
+        stars.push(star);
+      }
+
+      setInterval(animate, 750 / fps);
+    });
+
+    /**
+     * Animate the canvas
+     */
+    function animate() {
+      context.clearRect(0, 0, screenW, screenH);
+      $.each(stars, function () {
+        this.draw(context);
+      })
+    }
+
+    /**
+     * Star
+     * 
+     * @param int x
+     * @param int y
+     * @param int length
+     * @param opacity
+     */
+    function Star(x, y, length, opacity) {
+      this.x = parseInt(x);
+      this.y = parseInt(y);
+      this.length = parseInt(length);
+      this.opacity = opacity;
+      this.factor = 1;
+      this.increment = Math.random() * .03;
+    }
+
+    /**
+     * Draw a star
+     * 
+     * This function draws a start.
+     * You need to give the contaxt as a parameter 
+     * 
+     * @param context
+     */
+    Star.prototype.draw = function () {
+      context.rotate((Math.PI * 1 / 10));
+      context.save();
+      context.translate(this.x, this.y);
+
+      if (this.opacity > 1) {
+        this.factor = -1;
+      }
+      else if (this.opacity <= 0) {
+        this.factor = 1;
+
+        this.x = Math.round(Math.random() * screenW);
+        this.y = Math.round(Math.random() * screenH);
+      }
+
+      this.opacity += this.increment * this.factor;
+
+      context.beginPath()
+      for (var i = 5; i--;) {
+        context.lineTo(0, this.length);
+        context.translate(0, this.length);
+        context.rotate((Math.PI * 2 / 10));
+        context.lineTo(0, - this.length);
+        context.translate(0, - this.length);
+        context.rotate(-(Math.PI * 6 / 10));
+      }
+      context.lineTo(0, this.length);
+      context.closePath();
+      context.fillStyle = "rgba(255, 255, 200, " + this.opacity + ")";
+      context.shadowBlur = 5;
+      context.shadowColor = '#ffff33';
+      context.fill();
+
+      context.restore();
+    }
+
+    const imageUrls = [
+      '/LUR/OP/SIGILOS/A.png',
+      '/LUR/OP/SIGILOS/B.png',
+      '/LUR/OP/SIGILOS/C.png',
+      '/LUR/OP/SIGILOS/D.png',
+      '/LUR/OP/SIGILOS/E.png',
+      '/LUR/OP/SIGILOS/F.png',
+      '/LUR/OP/SIGILOS/G.png',
+      '/LUR/OP/SIGILOS/H.png',
+      '/LUR/OP/SIGILOS/I.png',
+      '/LUR/OP/SIGILOS/J.png',
+      '/LUR/OP/SIGILOS/K.png',
+      '/LUR/OP/SIGILOS/L.png',
+      '/LUR/OP/SIGILOS/M.png',
+      '/LUR/OP/SIGILOS/N.png',
+      '/LUR/OP/SIGILOS/O.png',
+      '/LUR/OP/SIGILOS/P.png',
+      '/LUR/OP/SIGILOS/Q.png',
+      '/LUR/OP/SIGILOS/R.png',
+      '/LUR/OP/SIGILOS/S.png',
+      '/LUR/OP/SIGILOS/T.png',
+      '/LUR/OP/SIGILOS/U.png',
+      '/LUR/OP/SIGILOS/V.png',
+      '/LUR/OP/SIGILOS/W.png',
+      '/LUR/OP/SIGILOS/X.png',
+      '/LUR/OP/SIGILOS/Y.png',
+      '/LUR/OP/SIGILOS/Z.png'
+    ];
+
+    function createRandomImage() {
+      const randomIndex = Math.floor(Math.random() * imageUrls.length);
+      const img = document.createElement('img');
+      img.src = imageUrls[randomIndex];
+      img.classList.add('random-image');
+
+      const x = Math.random() * (window.innerWidth - 100);
+      const y = Math.random() * (window.innerHeight - 100);
+      img.style.left = `${x}px`;
+      img.style.top = `${y}px`;
+
+      document.body.appendChild(img);
+
+      setTimeout(() => {
+        img.style.width = '100px';
+        img.style.height = '100px';
+        img.style.opacity = '0';
+      }, 100);
+
+      setTimeout(() => {
+        img.remove();
+      }, 2000);
+    }
+
+    setInterval(createRandomImage, 10);
+  </script>
+</body>
+
+</html>
+
+<style>
+  .random-image {
+    position: fixed;
+    width: 20px;
+    height: 20px;
+    transition: width 5s, height 5s, opacity 5s;
+    opacity: 0.85;
+    animation: shake2 0.5s ease-in-out infinite;
+    z-index: -10;
+    filter: brightness(0.4) saturate(10%) hue-rotate(50deg);
+    overflow: hidden;
+  }
+
+
+  @keyframes shake {
+    0% {
+      transform: translateX(0);
+    }
+
+    25% {
+      transform: translateX(-0.30px);
+    }
+
+    50% {
+      transform: translateX(0.30px);
+    }
+
+    75% {
+      transform: translateX(-0.30px);
+    }
+
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes shake2 {
+    0% {
+      transform: translateX(0);
+    }
+
+    25% {
+      transform: translateX(-1px);
+    }
+
+    50% {
+      transform: translateX(1px);
+    }
+
+    75% {
+      transform: translateX(-1px);
+    }
+
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes float {
+    0% {
+      transform: translateY(0);
+    }
+
+    50% {
+      transform: translateY(-10px);
+    }
+
+    100% {
+      transform: translateY(0);
+    }
+  }
+
+
+  @media (max-width: 768px) {
+    .header-container {
+      flex-direction: column;
+      text-align: center;
+    }
+
+    .back-button {
+      margin-top: 10px;
+      width: 100%;
+      box-sizing: border-box;
+    }
+
+    .main-content {
+      flex-direction: column;
+      padding: 0 10px;
+    }
+
+    .details-table {
+      width: 100%;
+    }
+
+    .details-image img {
+      max-width: 100%;
+    }
+  }
+
+  body {
+    font-family: "Roboto", sans-serif;
+    background-color: #fff;
+    color:rgb(155, 123, 20);
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .header {
+    background-color: #d4af37;
+    padding: 20px;
+    text-align: center;
+    transition: all 0.3s ease;
+  }
+
+  .header-small {
+    padding: 10px;
+  }
+
+  .header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  .site-title {
+    font-family: "Cinzel", serif;
+    color: #fff;
+    font-size: 2.2rem;
+  }
+
+  .back-button {
+    background-color: transparent;
+    color: #fff;
+    border: 2px solid #fff;
+    padding: 10px 20px;
+    font-size: 1rem;
+    cursor: pointer;
+    transition: all 0.3s ease;
+  }
+
+  .back-button:hover {
+    background-color: #fff;
+    color: #d4af37;
+    transform: scale(1.1);
+  }
+
+  .subheader {
+    background-color: #f9f6f0;
+    padding: 10px;
+    text-align: center;
+  }
+
+  .subheader nav ul {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+  }
+
+  .subheader nav ul li {
+    margin: 0 15px;
+  }
+
+  .subheader nav ul li a {
+    text-decoration: none;
+    color: #d4af37;
+    font-weight: 500;
+    transition: color 0.3s ease;
+  }
+
+  .subheader nav ul li a:hover {
+    color: #b8860b;
+  }
+
+  .main-content {
+    display: flex;
+    flex: 1;
+    max-width: 1600px;
+    margin: 20px auto;
+    gap: 20px;
+    padding: 0 10px;
+    flex-wrap: wrap;
+  }
+
+  .sidebar {
+    display: none;
+  }
+
+  .content {
+    flex: 1;
+    background-color: #f9f6f0;
+    padding: 20px;
+    border-radius: 8px;
+    font-size: 1.2rem;
+  }
+
+  h2 {
+    font-family: "Cinzel", serif;
+    color: rgb(214, 165, 4);
+    margin-bottom: 10px;
+    font-weight: bold;
+    font-size: 1.8rem;
+  }
+
+  .details-container {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .details-text {
+    display: flex;
+    gap: 20px;
+  }
+
+  .details-image {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  .details-image img {
+    width: 450px;
+    height: 450px;
+    border-radius: 8px;
+    transition: transform 0.3s ease, border-color 0.3s ease;
+  }
+
+  .details-image img:hover {
+    transform: scale(1.1);
+  }
+
+  .details-table {
+    width: 100%;
+    margin-top: 20px;
+    border-collapse: collapse;
+    margin-left: 0;
+    margin-right: auto;
+  }
+
+  .details-table th {
+    width: 40%
+  }
+
+  .details-table th,
+  .details-table td {
+    border: 1px solid #d4af37;
+    padding: 10px;
+    text-align: left;
+  }
+
+  .details-table th {
+    background-color: #d4af37;
+    color: #fff;
+  }
+
+  .details-table td {
+    background-color: #fff;
+    color: #d4af37;
+  }
+
+  #space {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+</style>
---
+++ OP/INVENTARIO/VALORES/Rituais/Sangue.php
@@ -0,0 +1,470 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title id="tituloPagina">Mistérios</title>
+  <link rel="stylesheet" href="styles.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Cinzel:wght@400;600&display=swap"
+    rel="stylesheet" />
+</head>
+
+<body>
+  <canvas id="c"></canvas>
+  <header class="header">
+    <div class="header-container">
+      <h1 class="site-title" id="RitualNome">[Nome do Ritual]</h1>
+      <button class="back-button" onclick="window.location.href='../index-inv.php'">
+        &larr; Voltar
+      </button>
+    </div>
+  </header>
+
+  <div class="subheader">
+    <nav>
+      <ul>
+        <li><a href="#efeito">Efeito</a></li>
+      </ul>
+    </nav>
+  </div>
+
+  <main class="main-content">
+    <aside class="sidebar"></aside>
+
+    <section class="content">
+      <article>
+      <div class="details-image">
+            <img
+              id="ritualImagem"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3r_sM7QQ45aSu9y8Bug6rJtMB--YWkyAiA&s"
+              alt="Imagem do Ritual"
+            />
+          </div>
+        <section id="efeito">
+          <h2 data-text="Efeito">Efeito</h2>
+          <p id="op_padrao" data-text="">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p id="op_discente" data-text=""><strong>Discente: </strong>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p id="op_verdadeiro" data-text=""><strong>Verdadeiro: </strong>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </section>
+        <br>
+
+        <section id="detalhes">
+          <table class="details-table">
+            <tr>
+              <th>Círculo</th>
+              <td id="RitualCirculo">[Círculo]</td>
+            </tr>
+            <tr>
+              <th>Custo</th>
+              <td id="RitualCusto">[Custo]</td>
+            </tr>
+            <tr>
+              <th>Alvo</th>
+              <td id="RitualAlvo">[Alvo]</td>
+            </tr>
+            <tr>
+              <th>Resistência</th>
+              <td id="RitualResistencia">[Resistência]</td>
+            </tr>
+            <tr>
+              <th>Duração</th>
+              <td id="RitualDuracao">[Duração]</td>
+            </tr>
+            <tr>
+              <th>Alcance</th>
+              <td id="RitualAlcance">[Alcance]</td>
+            </tr>
+          </table>
+          <br />
+          <div class="details-container">
+            <div class="details-text">
+              <p>
+                <strong>Tipo</strong>
+              </p>
+              <p>
+                <strong>Alcance</strong>
+              </p>
+              <p>
+                <strong>Efeito</strong>
+              </p>
+            </div>
+          </div>
+        </section>
+      </article>
+    </section>
+  </main>
+
+  <div class="blood-effect"></div>
+
+  <script>
+    window.onload = function (argument) {
+      var canvas = document.getElementById("c");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      if (canvas.getContext) {
+        var c = canvas.getContext("2d"),
+          w = canvas.width,
+          h = canvas.height;
+
+        c.strokeStyle = "red";
+        c.fillStyle = "#7a0a11";
+        c.lineWidth = 0.1;
+        var points = {
+          number: 333,
+          connect_distance: 50,
+          mouse_distance: 100,
+          array: [],
+        };
+
+        Point = function () {
+          this.x = Math.random() * w;
+          this.y = Math.random() * h;
+          this.vector_x = Math.random() - 0.5;
+          this.vector_y = Math.random() - 0.5;
+          this.speed_x = Math.random() * 20;
+          this.speed_y = Math.random() * 20;
+          this.radius = Math.random() * 5;
+        };
+
+        Point.prototype = {
+          draw_point: function () {
+            c.beginPath();
+            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+            c.fill();
+          },
+
+          next_position: function () {
+            var temp_p = this;
+
+            if (temp_p.x < 0 || temp_p.x > w) {
+              temp_p.vector_x = -temp_p.vector_x;
+            } else if (temp_p.y < 0 || temp_p.y > h) {
+              temp_p.vector_y = -temp_p.vector_y;
+            }
+
+            temp_p.x += temp_p.vector_x * temp_p.speed_x;
+            temp_p.y += temp_p.vector_y * temp_p.speed_y;
+          },
+        };
+
+        init();
+
+        connect_each = function () {
+          var temp_array = points.array;
+          var temp_queue = arguments[0];
+
+          for (var i = 0; i < points.number; i++) {
+            for (var j = 0; j < points.number; j++) {
+              pre_p = points.array[i];
+              next_p = temp_array[j];
+
+              if (
+                pre_p.x - next_p.x < points.connect_distance &&
+                pre_p.y - next_p.y < points.connect_distance &&
+                pre_p.x - next_p.x > -points.connect_distance &&
+                pre_p.y - next_p.y > -points.connect_distance
+              ) {
+                c.beginPath();
+                c.moveTo(pre_p.x, pre_p.y);
+                c.lineTo(next_p.x, next_p.y);
+                c.stroke();
+                c.closePath();
+              }
+            }
+          }
+        };
+        function init() {
+          for (var i = 0; i < points.number; i++) {
+            points.array.push(new Point());
+            points.array[i].draw_point();
+          }
+        }
+        function animated_part() {
+          var temp_array = points.array;
+          for (var i = 0; i < points.number; i++) {
+            temp_array[i].next_position();
+            temp_array[i].draw_point();
+          }
+          connect_each();
+        }
+        setInterval(function () {
+          animated_part();
+        }, 33);
+      }
+    };
+  </script>
+</body>
+
+</html>
+
+<style>
+  @import "compass/reset";
+  @import "compass/css3";
+
+  canvas {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgb(41, 0, 0);
+    z-index: -1;
+  }
+
+  @media (max-width: 768px) {
+    .header-container {
+      flex-direction: column;
+      text-align: center;
+    }
+
+    .back-button {
+      margin-top: 10px;
+      width: 100%;
+      box-sizing: border-box;
+    }
+
+    .main-content {
+      flex-direction: column;
+      padding: 0 10px;
+    }
+
+    .details-table {
+      width: 100%;
+    }
+
+    .details-image img {
+      max-width: 100%;
+    }
+  }
+
+  body {
+    font-family: "Roboto", sans-serif;
+    background-color: #6d0000;
+    color: #fff;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .header {
+    position: relative;
+    background-color: #550000;
+    padding: 20px;
+    text-align: center;
+    transition: all 0.3s ease;
+    opacity: 0.95;
+  }
+
+  .blood-effect {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100px;
+    background: linear-gradient(to top, #8b0000, transparent);
+    animation: blood-flow 5s infinite, pulse-effect 1.8s infinite ease-in-out;
+    z-index: 1;
+  }
+
+  @keyframes blood-flow {
+    0% {
+      background-position: 0% 0%;
+    }
+
+    50% {
+      background-position: 100% 100%;
+    }
+
+    100% {
+      background-position: 0% 0%;
+    }
+  }
+
+  @keyframes pulse-effect {
+    0% {
+      height: 95px;
+    }
+
+    50% {
+      height: 150px;/
+    }
+
+    100% {
+      height: 95px;
+    }
+  }
+
+  .header-small {
+    padding: 10px;
+  }
+
+  .header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+    position: relative;
+  }
+
+  .site-title {
+    font-family: "Cinzel", serif;
+    color: #fff;
+    font-size: 2.2rem;
+    text-shadow: 0 0 5px #ff0000;
+  }
+
+  .back-button {
+    background-color: transparent;
+    color: #fff;
+    border: 2px solid #ff0000;
+    padding: 10px 20px;
+    font-size: 1rem;
+    cursor: pointer;
+    transition: all 0.3s ease;
+  }
+
+  .back-button:hover {
+    background-color: #ff0000;
+    color: #fff;
+    transform: scale(1.1);
+  }
+
+  .subheader {
+    background-color: #440000;
+    padding: 10px;
+    text-align: center;
+  }
+
+  .subheader nav ul {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+  }
+
+  .subheader nav ul li {
+    margin: 0 15px;
+  }
+
+  .subheader nav ul li a {
+    text-decoration: none;
+    color: #fff;
+    font-weight: 500;
+    transition: color 0.3s ease;
+  }
+
+  .subheader nav ul li a:hover {
+    color: #ff0000;
+  }
+
+  .main-content {
+    display: flex;
+    flex: 1;
+    max-width: 1600px;
+    margin: 20px auto;
+    gap: 20px;
+    padding: 0 10px;
+    flex-wrap: wrap;
+    opacity: 0.95;
+  }
+
+  .sidebar {
+    display: none;
+  }
+
+  .content {
+    flex: 1;
+    background-color: #550000;
+    padding: 20px;
+    border-radius: 8px;
+    font-size: 1.2rem;
+  }
+
+  h2 {
+    font-family: "Cinzel", serif;
+    color: #ffe2e2;
+    margin-bottom: 10px;
+    font-weight: bold;
+    text-shadow: 0 0 5px #ff0000;
+    font-size: 1.8rem;
+  }
+
+  .details-container {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .details-text {
+    display: flex;
+    gap: 20px;
+  }
+
+  .details-image {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  .details-image img {
+    width: 450px;
+    height: 450px;
+    border-radius: 8px;
+    transition: transform 0.3s ease, border-color 0.3s ease;
+  }
+
+  .details-image img:hover {
+    transform: scale(1.1);
+  }
+
+  .details-table {
+    width: 100%;
+    margin-top: 20px;
+    border-collapse: collapse;
+    margin-left: 0;
+    margin-right: auto;
+  }
+
+  .details-table th {
+    width: 40%
+  }
+
+  .details-table th,
+  .details-table td {
+    border: 1px solid #ff5555;
+    padding: 10px;
+    text-align: left;
+  }
+
+  .details-table th {
+    background-color: #ff0000;
+    color: #fff;
+  }
+</style>
